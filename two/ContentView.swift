0b3b6ab2f8e10982fd690800e99cce4fd519d620
clo <dogloverr78@gmail.com>
2024-06-21 12:19:55 -0500

    Initial Commit

---
+++ two/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  two
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("What is your name?")
+                .font(.title)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.pink, width:10)
+            Button("Submit name") {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }}
+        .font(.title2)
+        .buttonStyle(.borderedProminent)
+        .tint(.pink)
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ two/twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  twoApp.swift
+//  two
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
